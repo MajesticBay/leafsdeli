43a1c3b28addabd6f34badaf204e48940452f9f1
Sergey <in43sh@gmail.com>
2019-11-07 00:27:07 -0800

    Add toggleMenu.js file.

---
+++ assets/js/toggleMenu.js
@@ -0,0 +1,13 @@
+function openMenu() {
+  let mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.style.width = "100%";
+  // if (mobileMenu.style.display === "flex") {
+  //   mobileMenu.style.display = "none";
+  // } else {
+  //   mobileMenu.style.display = "flex";
+  // }
+}
+function closeMenu() {
+  let mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.style.width = "0";
+}
