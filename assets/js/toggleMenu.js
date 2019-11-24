1cc36d7615d4d053440f5f1cb542af560dd09be5
in43sh <in43sh@gmail.com>
2019-11-24 00:41:08 -0800

    Work on top container and menu.

--- assets/js/toggleMenu.js
+++ assets/js/toggleMenu.js
@@ -1,11 +1,12 @@
 function openMenu() {
   let mobileMenu = document.getElementById("mobile-menu");
-  mobileMenu.style.width = "100%";
-  // if (mobileMenu.style.display === "flex") {
-  //   mobileMenu.style.display = "none";
-  // } else {
-  //   mobileMenu.style.display = "flex";
-  // }
+  // let mobileMenuList = document.getElementById("mobile-menu-list");
+  // mobileMenu.style.width = "100%";
+  if (mobileMenu.style.display === "flex") {
+    mobileMenu.style.display = "none";
+  } else {
+    mobileMenu.style.display = "flex";
+  }
 }
 function closeMenu() {
   let mobileMenu = document.getElementById("mobile-menu");
