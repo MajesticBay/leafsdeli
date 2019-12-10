752c10f87a2e116816d7f3be41b6691f70895ec1
Sergey <in43sh@gmail.com>
2019-12-09 23:03:46 -0800

    Work on the js functions to toggle the menu.

--- assets/js/toggleMenu.js
+++ assets/js/toggleMenu.js
@@ -2,13 +2,18 @@ function openMenu() {
   let mobileMenu = document.getElementById("mobile-menu");
   // let mobileMenuList = document.getElementById("mobile-menu-list");
   // mobileMenu.style.width = "100%";
-  if (mobileMenu.style.display === "flex") {
-    mobileMenu.style.display = "none";
-  } else {
-    mobileMenu.style.display = "flex";
-  }
+
+  // if (mobileMenu.style.display === "flex") {
+  //   mobileMenu.style.display = "none";
+  // } else {
+  //   mobileMenu.style.display = "flex";
+  // }
+  mobileMenu.style.display = "flex";
 }
 function closeMenu() {
+  // let mobileMenu = document.getElementById("mobile-menu");
+  // mobileMenu.style.width = "0";
   let mobileMenu = document.getElementById("mobile-menu");
-  mobileMenu.style.width = "0";
+  mobileMenu.style.display = "none";
+
 }
